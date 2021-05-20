3ae612ed8356a677c5e8b7b083688ebd797452b5
Cydney Pollard <sincerelycydney087@gmail.com>
2021-05-20 19:51:32 -0400

    added homepage to file system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
+import {BrowserRouter as Switch, Route} from 'react-router-dom';
 import NavBar from './NavigationBar/navbar.jsx';
-
+import HomePage from './Home/Home.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
       </div>
     )
   }
