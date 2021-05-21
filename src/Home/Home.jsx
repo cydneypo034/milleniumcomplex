f5d3c1777078aaf413fb10725bc4d19378b47aa5
Cydney Pollard <sincerelycydney087@gmail.com>
2021-05-21 05:18:55 -0400

    fixed parallax for content is centered for desktop, will be adjusting for mobile

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -27,54 +27,58 @@ export default class HomePage extends Component {
             bgImageAlt="gold-foil-paper"
             strength={200}>
 
+            <div style={{ height: '50px' }} />
+
+            <div className="row justify-content-center align-items-center h-100">
             <div className="card-wrapper">
-            <MDBCol>
-                    <MDBCard style={{ width: "22rem" }}>
-                        <MDBCardImage className="img-fluid" 
-                        src={FineArt} waves />
-                        <MDBCardBody className="card-info">
-                        <MDBCardTitle>The ArtfulCreations Shop</MDBCardTitle>
-                        <MDBCardText>
-                            Visit our fine art shop to shop for one-of-a-kind, 
-                            Holy Spirit inspired artwork created in house by owner and prophetic fine artist Cydney Pollard.
-                        </MDBCardText>
-                        <MDBBtn color="brown" href="#">Shop ArtfulCreations</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
+                        <MDBCol>
+                                <MDBCard style={{ width: "22rem" }}>
+                                    <MDBCardImage className="img-fluid" 
+                                    src={FineArt} waves />
+                                    <MDBCardBody className="card-info">
+                                    <MDBCardTitle>The ArtfulCreations Shop</MDBCardTitle>
+                                    <MDBCardText>
+                                        Visit our fine art shop to shop for one-of-a-kind, 
+                                        Holy Spirit inspired artwork created in house by owner and prophetic fine artist Cydney Pollard.
+                                    </MDBCardText>
+                                    <MDBBtn color="brown" href="#">Shop ArtfulCreations</MDBBtn>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
 
-                <MDBCol>
-                    <MDBCard style={{ width: "22rem" }}>
-                        <MDBCardImage className="img-fluid" 
-                        src={BrandDesign} waves />
-                        <MDBCardBody className="card-info">
-                        <MDBCardTitle>Brand Design and Strategy</MDBCardTitle>
-                        <MDBCardText>
-                            *Coming Soon* Feel free to schedule a brand audit or design clarity call with us for 
-                            your branding needs in both strategy and design.
-                        </MDBCardText>
-                        <MDBBtn color="brown" href="#">View Our Calendar</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
+                            <MDBCol>
+                                <MDBCard style={{ width: "22rem" }}>
+                                    <MDBCardImage className="img-fluid" 
+                                    src={BrandDesign} waves />
+                                    <MDBCardBody className="card-info">
+                                    <MDBCardTitle>Brand Design and Strategy</MDBCardTitle>
+                                    <MDBCardText>
+                                        *Coming Soon* Feel free to schedule a brand audit or design clarity call with us for 
+                                        your branding needs in both strategy and design.
+                                    </MDBCardText>
+                                    <MDBBtn color="brown" href="#">View Our Calendar</MDBBtn>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
 
-                <MDBCol>
-                    <MDBCard style={{ width: "22rem" }}>
-                        <MDBCardImage className="img-fluid" 
-                        src={LearnMore} waves />
-                        <MDBCardBody className="card-info">
-                        <MDBCardTitle>Prophetic Art Education</MDBCardTitle>
-                        <MDBCardText>
-                            *Coming Soon* Check out our blog to read on the prophetic arts, what it all entails, and exercises
-                            to help in your prophetic exploration.
-                        </MDBCardText>
-                        <MDBBtn color="brown" href="#">See Our Blog</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
+                            <MDBCol>
+                                <MDBCard style={{ width: "22rem" }}>
+                                    <MDBCardImage className="img-fluid" 
+                                    src={LearnMore} waves />
+                                    <MDBCardBody className="card-info">
+                                    <MDBCardTitle>Prophetic Art Education</MDBCardTitle>
+                                    <MDBCardText>
+                                        *Coming Soon* Check out our blog to read on the prophetic arts, what it all entails, and exercises
+                                        to help in your prophetic exploration.
+                                    </MDBCardText>
+                                    <MDBBtn color="brown" href="#">See Our Blog</MDBBtn>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                        </div>
             </div>
 
-            <div style={{ height: '100px' }} />
+            <div style={{ height: '50px' }} />
             </Parallax>
             </div>
         )
