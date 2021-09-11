586fb07d0e08b05aff210a9c45f8b7179ce4ae90
Cydney Pollard <sincerelycydney087@gmail.com>
2021-09-11 05:31:15 -0400

    centered form in footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,30 +1,28 @@
 import React, {Component} from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBInput, MDBIcon, MDBFooter, MDBBtn } from "mdbreact";
+import ForIAmLogo2 from '../images/foriamlogo2.png';
 
 export default class FooterPage extends Component {
     render() {
         return (
             <MDBFooter color="#1a237e indigo darken-4" className="font-small pt-4 mt-4">
-              <MDBContainer fluid className="text-center text-md-left">
-              <MDBRow>
+              <MDBContainer fluid className="text-center text-md-center">
+              
+                <div className="form">
+                  <p className="h4 text-center">Stay Connected</p>
+                  <div class="input-group">
+                  <MDBInput type="text" className="form-control" name="name" label="Enter Your Name" color="white"></MDBInput> &nbsp;
+                  <MDBInput type="text" className="form-control" name="email" label="Enter Your Email" color="white"></MDBInput>
+                  <span class="input-group-btn">
+                    <MDBBtn class="btn" type="button" color="white">Send</MDBBtn>
+                  </span>
+                </div>
+                </div>
+                  
+                  
+        
 
-                  <MDBCol md="6">
-                  <form className="form-group w-50">
-                  <p className="h5 text-left mb-4">Stay Connected</p>
-                  <h6 className="footer-text">Sign up to stay updated with all updates released!</h6>
-                  <div className="grey-text">
-                    <MDBInput label="Enter Your Email Here" group type="text" validate error="wrong"
-                      success="right" />
-                  </div>
-                  <div className="button-styled">
-                    <MDBBtn color="white">
-                      Send
-                      <MDBIcon far icon="paper-plane" className="ml-1" />
-                    </MDBBtn>
-                    </div>
-                      </form>
-                    </MDBCol>
-                </MDBRow>
+                <a href="#!"><img src={ForIAmLogo2} style={{ width: "10rem", height: "10rem" }}/></a>
                 </MDBContainer>
 
 
@@ -40,6 +38,8 @@ export default class FooterPage extends Component {
                 </MDBContainer>
               </div>
             </MDBFooter>
+
+            
       );
     }
 }
