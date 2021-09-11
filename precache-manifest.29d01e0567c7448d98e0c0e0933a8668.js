44cca055bf101c1285f6a2e8cda6e2c2eea06d9e
Cydney Pollard <sincerelycydney087@gmail.com>
2021-09-11 06:38:36 -0400

    Updates

--- precache-manifest.29d01e0567c7448d98e0c0e0933a8668.js
+++ precache-manifest.29d01e0567c7448d98e0c0e0933a8668.js
@@ -72,28 +72,28 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/blueparallax.4ce5e5c7.jpg"
   },
   {
-    "revision": "f7307680c7fe85959f3ecf122493ea7d",
-    "url": "/milleniumcomplex/static/media/fa-brands-400.f7307680.woff2"
-  },
-  {
     "revision": "30cc681d4487d2f561035ba24a68c629",
     "url": "/milleniumcomplex/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/milleniumcomplex/static/media/fa-brands-400.f7307680.woff2"
   },
   {
     "revision": "099a9556e1a63ece24f8a99859c94c7d",
     "url": "/milleniumcomplex/static/media/fa-brands-400.099a9556.woff"
   },
   {
-    "revision": "f0f8230116992e521526097a28f54066",
-    "url": "/milleniumcomplex/static/media/fa-regular-400.f0f82301.woff2"
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/milleniumcomplex/static/media/fa-brands-400.3b89dd10.ttf"
   },
   {
     "revision": "7630483dd4b0c48639d2ac54a894b450",
     "url": "/milleniumcomplex/static/media/fa-regular-400.7630483d.eot"
   },
   {
-    "revision": "3b89dd103490708d19a95adcae52210e",
-    "url": "/milleniumcomplex/static/media/fa-brands-400.3b89dd10.ttf"
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/milleniumcomplex/static/media/fa-regular-400.f0f82301.woff2"
   },
   {
     "revision": "7124eb50fc8227c78269f2d995637ff5",
@@ -108,16 +108,16 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/fa-regular-400.0bb42845.svg"
   },
   {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/milleniumcomplex/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
     "revision": "e8a427e15cc502bef99cfd722b37ea98",
     "url": "/milleniumcomplex/static/media/fa-solid-900.e8a427e1.woff2"
   },
   {
     "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
     "url": "/milleniumcomplex/static/media/fa-solid-900.9fe5a17c.woff"
-  },
-  {
-    "revision": "1042e8ca1ce821518a2d3e7055410839",
-    "url": "/milleniumcomplex/static/media/fa-solid-900.1042e8ca.eot"
   },
   {
     "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
@@ -132,24 +132,24 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/fa-solid-900.376c1f97.svg"
   },
   {
-    "revision": "954bbdeb86483e4ffea00c4591530ece",
-    "url": "/milleniumcomplex/static/media/Roboto-Thin.954bbdeb.woff2"
-  },
-  {
     "revision": "dfe56a876d0282555d1e2458e278060f",
     "url": "/milleniumcomplex/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/milleniumcomplex/static/media/Roboto-Thin.954bbdeb.woff2"
   },
   {
     "revision": "7500519de3d82e33d1587f8042e2afcb",
     "url": "/milleniumcomplex/static/media/Roboto-Thin.7500519d.woff"
   },
   {
-    "revision": "94998475f6aea65f558494802416c1cf",
-    "url": "/milleniumcomplex/static/media/Roboto-Thin.94998475.ttf"
-  },
-  {
     "revision": "a990f611f2305dc12965f186c2ef2690",
     "url": "/milleniumcomplex/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/milleniumcomplex/static/media/Roboto-Thin.94998475.ttf"
   },
   {
     "revision": "69f8a0617ac472f78e45841323a3df9e",
@@ -164,20 +164,16 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/Roboto-Regular.30799efa.eot"
   },
   {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/milleniumcomplex/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
     "revision": "2751ee43015f9884c3642f103b7f70c9",
     "url": "/milleniumcomplex/static/media/Roboto-Regular.2751ee43.woff2"
   },
   {
     "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
     "url": "/milleniumcomplex/static/media/Roboto-Regular.ba3dcd89.woff"
-  },
-  {
-    "revision": "46e48ce0628835f68a7369d0254e4283",
-    "url": "/milleniumcomplex/static/media/Roboto-Light.46e48ce0.ttf"
-  },
-  {
-    "revision": "df7b648ce5356ea1ebce435b3459fd60",
-    "url": "/milleniumcomplex/static/media/Roboto-Regular.df7b648c.ttf"
   },
   {
     "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
@@ -188,6 +184,10 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/Roboto-Medium.574fd0b5.woff2"
   },
   {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/milleniumcomplex/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
     "revision": "fc78759e93a6cac50458610e3d9d63a0",
     "url": "/milleniumcomplex/static/media/Roboto-Medium.fc78759e.woff"
   },
@@ -196,12 +196,12 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/Roboto-Bold.ecdd509c.eot"
   },
   {
-    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
-    "url": "/milleniumcomplex/static/media/Roboto-Medium.894a2ede.ttf"
-  },
-  {
     "revision": "39b2c3031be6b4ea96e2e3e95d307814",
     "url": "/milleniumcomplex/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/milleniumcomplex/static/media/Roboto-Medium.894a2ede.ttf"
   },
   {
     "revision": "dc81817def276b4f21395f7ea5e88dcd",
@@ -212,12 +212,12 @@ self.__precacheManifest = [
     "url": "/milleniumcomplex/static/media/arrow_left.11911410.svg"
   },
   {
-    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
-    "url": "/milleniumcomplex/static/media/Roboto-Bold.e31fcf18.ttf"
-  },
-  {
     "revision": "8ef6a08cdc1154920165680a4edde771",
     "url": "/milleniumcomplex/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/milleniumcomplex/static/media/Roboto-Bold.e31fcf18.ttf"
   },
   {
     "revision": "627acd0f6701a09c56b2c1388ee5ced6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/milleniumcomplex/precache-manifest.29af3df8c646db1cd41a36506e45ac44.js"
+  "/milleniumcomplex/precache-manifest.29d01e0567c7448d98e0c0e0933a8668.js"
 );
 
 workbox.clientsClaim();
