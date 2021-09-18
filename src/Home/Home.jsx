4b71582651a7c6e7503a84f98a0217f52b4cee48
Cydney Love <cydneypollard@iMac.lan>
2021-09-18 19:40:44 -0400

    added new photos, and adjusted video blurb

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player';
 import McParallax from '../Parallax/mcparallax.jsx';
 import MCCarousel from "../Carousel/carousel.jsx";
 import Background from "../images/bluewaves.jpg";
-import SolomonPhoto from "../images/solomonphoto.jpg";
+import SolomonPhoto from "../images/DrPBassman.jpg";
 
 export default class HomePage extends Component {
     render() {
@@ -13,37 +13,31 @@ export default class HomePage extends Component {
 
             
             <h1 style={{ color: "white", backgroundColor: "#1a237e", padding: "1rem"}}> WHO ARE WE </h1>
-
-
             <div class="jumbotron text-center hoverable p-4">
                 <div class="row">
 
                     <div class="col-md-4 offset-md-1 mx-3 my-3">
-                    <div class="view overlay">
                         <img src={SolomonPhoto} class="img-fluid" alt="blog-list" />  
                     </div>
-                    </div>
-
 
                     <div class="col-md-7 text-md-left ml-3 mt-3">
+
                     <h4 class="h4 mb-4">VISION STATEMENT</h4>
-                    
                     <p class="font-weight-normal">Our vision is to be of service as believers working 
                     together for the common purpose of spreading the gospel of Jesus Christ collectively 
                     through worship and praise, teaching and preaching using His pulpit, 
                     and glorifying Him through artistic media.</p>
 
                     <h4 class="h4 mb-4">MISSION STATEMENT</h4>
-
                     <p class="font-weight-normal">The mission of For I Am Global Ministries is to honor the Father by inviting lost souls 
                     into salvation through the blood of our savior Jesus Christ and to encourage the saints through 
                     fellowship. We believe there is one and only one true and living God (I Am) 
                     who is self-existent and His son Jesus Christ, the creator of heaven and Earth, 
                     the redeemer of mankind.</p>
+
                     </div>
 
                 </div>
-
                 </div>
 
             <h1 style={{ color: "white", backgroundColor: "#1a237e", padding: "1rem"}}> OUR MINISTRIES </h1>
@@ -51,20 +45,23 @@ export default class HomePage extends Component {
 
 
             <h1 style={{ color: "white", backgroundColor: "#1a237e", padding: "1rem"}}> FIAWORLD GOSPEL PRODUCTIONS</h1>
+            
+            <div class="jumbotron text-center hoverable p-4">
             <div class="row">
-            <div style={{padding:"2rem", backgroundImage: `url(${Background})`}}>
             
             <div class="col-md-4 offset-md-1 mx-3 my-3">
             <ReactPlayer url='https://www.youtube.com/embed/HYqMoW48tDs' playing='true' loop='true'/>
             </div>
 
-             <div class="col-md-7 text-md-left ml-3 mt-3" style={{color: "white"}}>
+             <div class="col-md-7 text-md-left ml-3 mt-3" >
+             <h4 class="h4 mb-4">FIA Corporation</h4>
+             <p class="font-weight-normal">
              FIA Corporation has been serving the Atlanta community in music as well as video broadcasting and production since 1993.
              While the company thrives in many entertainment categories, some of the current corporate subsidiaries include: TV Broadcast Productions, 
-             Music and Artist Productions, KCS-Squared Film and Animations, Studio Software, and WFIA Hybrid Radio Station.
+             Music and Artist Productions, KCS-Squared Film and Animations, Studio Software, and WFIA Hybrid Radio Station.</p>
+             </div>
              </div>
 
-            </div>
             </div>
 
             
--- src/Parallax/mcparallax.jsx
+++ src/Parallax/mcparallax.jsx
@@ -3,6 +3,8 @@ import { MDBBtn, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCol, MDBCar
 import FIAMUSIC from '../images/fiagospelorigin.png';
 import KCSSquared from '../images/kcssquaredorigin.png';
 import Positiv from '../images/positivorigin.png';
+import BibleStudy from '../images/biblestudy.png';
+import MinistryHands from '../images/ministryhands.png';
 import { Parallax } from 'react-parallax';
 
 export default class MCParallex extends Component {
@@ -15,7 +17,7 @@ export default class MCParallex extends Component {
             bgImageAlt="blue-playstation-background"
             strength={200}>
 
-            <div style={{ height: '40px' }} />
+            <div style={{ height: '50px' }} />
             <div className="row justify-content-center align-items-center h-100" style={{padding:"2rem"}}>
             <div className="card-wrapper">
                         <MDBCol>
@@ -53,33 +55,33 @@ export default class MCParallex extends Component {
                                     <MDBCardTitle>Inspiration</MDBCardTitle>
                                     <MDBCardTitle className="subtitle-text-card">Positiv Newsletter</MDBCardTitle>
                                     <MDBCardText>
-                                        A Christian quarterly news and inspiration publication with words to inspire and encourage
+                                        A Christian quarterly news and inspiration publication meant to inspire and encourage
                                     </MDBCardText>
-                                    <MDBBtn color="#1a237e indigo darken-4" className="white-text" href="#">View Our Recent Article</MDBBtn>
+                                    <MDBBtn color="#1a237e indigo darken-4" className="white-text" href="#">Read Our Articles</MDBBtn>
                                     </MDBCardBody>
                                 </MDBCard>
                             </MDBCol>
 
                             <MDBCol>
                                 <MDBCard style={{ width: "22rem", padding: "2rem" }}>
-                                <MDBCardImage className="img-fluid" src={Positiv} waves />
+                                <MDBCardImage className="img-fluid" src={BibleStudy} waves />
                                     <MDBCardBody className="card-info">
                                     <MDBCardTitle>Bible Study</MDBCardTitle>
-                                    <MDBCardTitle className="subtitle-text-card">Positiv Newsletter</MDBCardTitle>
+                                    <MDBCardTitle className="subtitle-text-card">Weekly Fellowship</MDBCardTitle>
                                     <MDBCardText>
                                         Join us for Bible Studies live on Zoom for words of encouragement and preaching of the Gospel.
                                     </MDBCardText>
-                                    <MDBBtn color="#1a237e indigo darken-4" className="white-text" href="#">View When We Preach</MDBBtn>
+                                    <MDBBtn color="#1a237e indigo darken-4" className="white-text" href="#">Watch Us Preach</MDBBtn>
                                     </MDBCardBody>
                                 </MDBCard>
                             </MDBCol>
 
                             <MDBCol>
                                 <MDBCard style={{ width: "22rem", padding: "2rem" }}>
-                                <MDBCardImage className="img-fluid" src={Positiv} waves />
+                                <MDBCardImage className="img-fluid" src={MinistryHands} waves />
                                     <MDBCardBody className="card-info">
                                     <MDBCardTitle>Ministry</MDBCardTitle>
-                                    <MDBCardTitle className="subtitle-text-card">Positiv Newsletter</MDBCardTitle>
+                                    <MDBCardTitle className="subtitle-text-card">Helping Hands</MDBCardTitle>
                                     <MDBCardText>
                                         We serve because He came to serve us first. See which ministry you'd like to be apart of.
                                     </MDBCardText>
